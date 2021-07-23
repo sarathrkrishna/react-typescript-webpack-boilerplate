6ad9974ddbcec116722e5e17749d755e49fa9351
SarathAR <sarathradhakrishnan@qburst.com>
2021-07-23 12:16:00 +0530

    setup for prod and dev states

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,4 @@
 import ReactDOM from "react-dom";
 import "./style.scss";
-import IMAGE from "./image.png";
-import VECTOR from "./vector.svg";
 
-ReactDOM.render(
-  <div>
-    <img src={IMAGE} alt="" />
-    <img src={VECTOR} alt="" />
-  </div>,
-  document.getElementById("app")
-);
+ReactDOM.render(<div>Hello world</div>, document.getElementById("app"));
